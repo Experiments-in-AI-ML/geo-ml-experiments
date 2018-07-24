6a93b64aad733c7555ff07714e2aaab10a55cb08
Alex Ramos <atramos@gmail.com>
2018-07-23 19:17:41 -0700

    wip nn for classification task, posted to stackoverflow

--- California_Geo_ML_Experiment/poc4-address-lat-lon/neural_network.py
+++ California_Geo_ML_Experiment/poc4-address-lat-lon/neural_network.py
@@ -3,37 +3,45 @@ import numpy as np
 
 # hard-coded input and labels for demonstration
 training_x = np.array([[1.], [2.],[3.],[4.],[5.]])
-labels_training = np.array([[1.],[4.],[9.],[16.],[25.]])
+labels_training = np.array([[1,0],[0,1],[1,0],[0,1],[1,0]])
+
+print('Input:\n' + str(training_x))
+print('Training Output:\n' + str(labels_training))
 
 # Hyperparameters
-num_epochs = 50000
+num_epochs = 100000
 learning_rate = 0.001
-LAYERS = 3
+HIDDEN = 20
+
+# scan data
+INPUT_SAMPLES = len(training_x)
+INPUT_VARS = len(training_x[0])
+OUTPUT_CLASSES = len(labels_training[0])
+OUTPUT_SAMPLES = len(labels_training)
+print("Input samples = " + str(INPUT_SAMPLES) + ", OUTPUT_CLASSES=" + str(OUTPUT_CLASSES) + ", OUTPUT_SAMPLES=" + str(OUTPUT_SAMPLES))
 
 # setup the Neural Network
-INPUT = len(training_x)
-OUTPUT = len(labels_training)
-X = tf.placeholder(tf.float32, shape=[INPUT,None])
-Y = tf.placeholder(tf.float32, shape=[OUTPUT, None])
+X = tf.placeholder(tf.float32, shape=[None, INPUT_VARS])
+Y = tf.placeholder(tf.float32, shape=[None, OUTPUT_CLASSES])
 parameters = {
-    'W1': tf.Variable(np.random.randn(LAYERS,INPUT), dtype=tf.float32),
-    'b1': tf.Variable(np.zeros([LAYERS,1]), dtype=tf.float32),
-    'W2': tf.Variable(np.random.randn(OUTPUT,LAYERS), dtype=tf.float32),
-    'b2': tf.Variable(np.zeros([OUTPUT,1]), dtype=tf.float32)
+    'W1': tf.Variable(tf.random_normal([INPUT_VARS, HIDDEN])),
+    'b1': tf.Variable(tf.random_normal([HIDDEN])),
+    'W2': tf.Variable(tf.random_normal([HIDDEN, OUTPUT_CLASSES])),
+    'b2': tf.Variable(tf.random_normal([OUTPUT_CLASSES]))
 }
 
-Z1 = tf.add(tf.matmul(parameters['W1'], X), parameters['b1']) # W1*X + b
-#Z1 = tf.add(tf.matmul(X, parameters['W1']), parameters['b1'])
+print('W1: ' + str(parameters['W1']))
+print('b1: ' + str(parameters['b1']))
+print('X: ' + str(X))
+print('W2: ' + str(parameters['W2']))
+print('b2: ' + str(parameters['b2']))
+print('Y: ' + str(Y))
 
+Z1 = tf.add(tf.matmul(X, parameters['W1']), parameters['b1'])
 A2 = tf.nn.relu(Z1)
+Z2 = tf.add(tf.matmul(A2, parameters['W2']), parameters['b2']) 
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Z2,  labels=Y))
 
-Z2 = tf.add(tf.matmul(parameters['W2'], A2), parameters['b2']) 
-#Z2 = tf.add(tf.matmul(A2, parameters['W2'], A2), parameters['b2']) 
-
-#cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Z2,  labels=Y))
-#cost = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(Z2, A2))))
-cost = tf.reduce_mean(tf.losses.mean_squared_error(labels = Y, predictions = Z2))
- 
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 init = tf.global_variables_initializer()
 
@@ -46,7 +54,7 @@ with tf.Session() as sess:
         if epoch % 200 == 0:
             print ("Cost after epoch %i: %f" % (epoch, c))
 
-    # Test predictions again by computing the output using training set as input again
+    # Test predictions by computing the output using training set as input
     output = sess.run(Z2, feed_dict={X: training_x})
     print(np.array2string(output, precision=3))
     
